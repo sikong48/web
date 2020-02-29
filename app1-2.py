562d16bdf3fabe837a519e496dd8a7574e801d3b
shikunming <13641018367@163.com>
2020-02-29 16:07:11 +0800

    application拆分出request、response。

---
+++ app1-2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from urllib.parse import parse_qs
+from html import escape
+
+class Request:
+#    params = parse_qs(environ.get('QUERY_STRING'))
+#    name = params.get('NAME', ['anon'])[0]
+    def __init__(self, environ):
+        self.params = parse_qs(environ.get('QUERY_STRING'))
+
+class Response:
+    STATUS = {
+        200: 'OK',
+        404: 'Not found'
+    }
+    def __init__(self, body=None):
+        self.body = body
+        self.status = '200 OK'
+        self.headers = {
+
+        }
+    def set_body(self, body):
+        self.body = body
+        self.headers['content-length'] = str(len(self.body))
+    def set_status(self, status_code, status_test):
+        self.status = '{} {}'.format(status_code, status_test)
+    def set_headers(self, name, value):
+        self.headers[name] = value
+
+    def __call__(self, start_response):
+        start_response(self.status, [(k, v) for k, v in self.headers.items()])
+        return [self.body.encode()]
+
+
+def application(environ, start_response):
+    request = Request(environ)
+    name = request.params.get('name', ['anont'])[0]
+    body = 'hello {}'.format(escape(name))
+    return Response(body)(start_response)
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    server = make_server('0.0.0.0', 3000, application)
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.shutdown()
